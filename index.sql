9147d6e7a57212bd3b0d3096073880a8f6d79788
mattwoodruff808 <mjwoodruff93@gmail.com>
2021-01-15 16:30:22 -0700

    Finished practice nested queries section

--- index.sql
+++ index.sql
@@ -51,3 +51,56 @@ WHERE g.name = 'Alternative & Punk';
 ---------------------------
 
 
+-- Practice nested queries
+
+-- Problem 1
+SELECT *
+FROM invoice
+WHERE invoice_id IN (SELECT invoice_id FROM invoice_line WHERE unit_price > 0.99);
+
+-- Problem 2
+SELECT *
+FROM playlist_track
+WHERE playlist_id IN (SELECT playlist_id FROM playlist WHERE name = 'Music');
+
+-- Problem 3
+SELECT name
+FROM track
+WHERE track_id IN (SELECT track_id FROM playlist_track WHERE playlist_id = 5);
+
+-- Problem 4
+SELECT *
+FROM track
+WHERE album_id IN (SELECT album_id FROM album WHERE title = 'Fireball');
+
+-- Problem 5
+SELECT *
+FROM track
+WHERE album_id IN (SELECT album_id FROM album WHERE title = 'Fireball');
+
+-- Problem 6
+SELECT *
+FROM track
+WHERE album_id IN (
+  SELECT album_id FROM album WHERE artist_id IN (
+    SELECT artist_id FROM artist WHERE name = 'Queen'
+  )
+);
+
+---------------------------
+
+
+-- Practice updating Rows
+
+-- Problem 1
+
+
+-- Problem 2
+
+-- Problem 3
+
+-- Problem 4
+
+-- Problem 5
+
+-- Problem 6
