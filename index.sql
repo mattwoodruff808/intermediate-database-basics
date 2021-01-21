6a871aa1170f0658baf901d91c9880de6607208b
mattwoodruff808 <mjwoodruff93@gmail.com>
2021-01-20 21:57:33 -0700

    Finished Group by

--- index.sql
+++ index.sql
@@ -124,6 +124,30 @@ AND composer IS null;
 -- Group by
 
 -- Problem 1
+SELECT COUNT(*), g.name
+FROM track t
+JOIN genre g ON t.genre_id = g.genre_id
+GROUP BY g.name;
+
+-- Problem 2
+SELECT COUNT(*), g.name
+FROM track t
+JOIN genre g ON t.genre_id = g.genre_id
+WHERE g.name = 'Pop' OR g.name = 'Rock'
+GROUP BY g.name;
+
+-- Problem 3
+SELECT ar.name, COUNT(*)
+FROM album al
+JOIN artist ar ON ar.artist_id = al.artist_id
+GROUP BY ar.name;
+
+---------------------------
+
+
+-- Use Distinct
+
+-- Problem 1
 
 
 -- Problem 2
